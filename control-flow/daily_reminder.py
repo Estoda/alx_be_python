6ca6c7333fe58271fd0a9f44f2b5caf4b80a6aaf
root <root@EsToDa.localdomain>
2024-06-26 17:10:34 +0300

    26

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,30 +2,15 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-reminder ="'" +  str(task) + "' is a " + str(priority) + " priority task" 
-
 match priority:
     case "high":
-        if time_bound == "yes":
-            reminder += " that requires immediate attention today!"
-            print("Reminder:", reminder)
-        elif time_bound == "no":
-            reminder += ". Consider completing it when you have free time."
-            print("Reminder:", reminder)
-    case "low":
-        reminder = "Note:" + reminder
-        if time_bound == "yes":
-            reminder += " that requires immediate attention today!"
-            print("Note:", reminder)
-        elif time_bound == "no":
-            reminder += ". Consider completing it when you have free time." 
-            print("Note:", reminder)
-    case "medium":
-        reminder = "Note: " + reminder
-        if time_bound == "yes":
-            reminder += " that requires immediate attention today!"
-            print("Note:", reminder)
-        elif time_bound == "no":
-            reminder += ". Consider completing it when you have free time." 
-            print("Note:", reminder)
+        reminder = f"Reminder: '{task}' is a high priority task"
+    case _:
+        reminder = f"Note: '{task}' is a {priority} task"
+if time_bound == "yes":
+    reminder += " that requires immediate attention today!"
+else:
+    reminder += ". Consider completing it when you have free time."
+
+print(reminder)
 

deebad70e7ac387dcf46cce7f5b3ea8d41ae4cb9
root <root@EsToDa.localdomain>
2024-06-26 05:13:47 +0300

    ddd

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,32 +2,30 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-reminder ="'" + str(task) + "' is a " + str(priority) + " priority task." 
+reminder ="'" +  str(task) + "' is a " + str(priority) + " priority task" 
 
 match priority:
     case "high":
-        reminder = "Reminder:", reminder
         if time_bound == "yes":
             reminder += " that requires immediate attention today!"
-            print(reminder)
+            print("Reminder:", reminder)
         elif time_bound == "no":
-            reminder += " Consider completing it when you have free time."
-            print(reminder)
+            reminder += ". Consider completing it when you have free time."
+            print("Reminder:", reminder)
     case "low":
+        reminder = "Note:" + reminder
+        if time_bound == "yes":
+            reminder += " that requires immediate attention today!"
+            print("Note:", reminder)
+        elif time_bound == "no":
+            reminder += ". Consider completing it when you have free time." 
+            print("Note:", reminder)
+    case "medium":
         reminder = "Note: " + reminder
         if time_bound == "yes":
             reminder += " that requires immediate attention today!"
-            print(reminder)
+            print("Note:", reminder)
         elif time_bound == "no":
-            reminder += " Consider completing it when you have free time." 
-            print(reminder)
-    case "medium":
-        reminder = "Note:", reminder
-        if time_bound == "yes":
-            reminder += " that requires immediate attention today!"
-            print(reminder)
-        elif time_bound == "no":
-            reminder += " Consider completing it when you have free time." 
-            print(reminder)
-    case _:
-            print("Non")
+            reminder += ". Consider completing it when you have free time." 
+            print("Note:", reminder)
+

75c53fa06a8e6f771835faa7e68252778efaf788
root <root@EsToDa.localdomain>
2024-06-26 04:04:05 +0300

    done

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,25 +1,21 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 oper = input("Choose the operation (+, -, *, /): ")
-result = 0
-end = True
 
 match oper:
-    case "+":
-        result = num1 + num2
-    case "-":
-        result = num1 - num2
-    case "*":
-        result = num1 * num2
-    case "/":
+    case '+':
+        print("The result is", str(num1 + num2) + '.')
+    case '-':
+        print("The result is", str(num1 - num2) + '.')
+    case '*':
+        print("The result is", str(num1 * num2) + '.')
+
+    case '/':
         if num2 == 0:
             print("Cannot dividde by zero.")
-            end = False
         else:
-            result = num1 / num2
+            print("The result is", str(num1 / num2) + '.')
     case _:
         print("Wrong input.")
 
-if(end):
-    print(f"The result is {result}.")
 

b0b7e97f7c1fc51206d4ae0f205379dcd6a68a75
root <root@EsToDa.localdomain>
2024-06-26 04:17:43 +0300

    dododd

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
-for n in range(1, 11):
-    print(f"{number} * {n} = {number * n}")
+for i in range(1, 11):
+    print(f"{number} * {i} = {number * i}")

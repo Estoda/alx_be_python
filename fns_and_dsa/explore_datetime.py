b4282b771efe4a7ff3d3ce4b995bbf1b9471c69f
root <root@EsToDa.localdomain>
2024-07-04 09:53:51 +0300

    2 done

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,14 @@
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {current_date}")
+
+def calculate_future_date():
+    num_days = int(input("Enter the number of days to add to the current date: "))
+    future_date = (datetime.datetime.now() + datetime.timedelta(days=num_days)).strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Future date: {future_date}")
+
+
+display_current_datetime()
+calculate_future_date()

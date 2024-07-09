d58b755fcba3283a8cec26090a5ddf30bb9eeaa8
root <root@EsToDa.localdomain>
2024-07-09 19:44:17 +0300

    done

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,7 +15,7 @@ class BankAccount:
             return False
     
     def display_balance(self):
-        print(f"Your balance: {self.account_balance}")
+        print(f"Current Balance: {self.account_balance}")
     
     
 

53b114724f0f1e8caf02a1a3d4c32f37fddc546c
root <root@EsToDa.localdomain>
2024-07-10 17:05:20 +0300

    done

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,32 @@
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.__is_checked_out = True
+    
+
+class Library:
+    __books = []
+    
+    def add_book(self, book: Book):
+        self.__books.append(book)
+
+    def list_available_books():
+        for b in Library.__books:
+            b: Book
+            if b.__is_checked_out:
+                print(f"{b.title} by {b.author}")
+
+    def check_out_book(title):
+        for b in Library.__books:
+            b: Book
+            if b.title == title:
+                b.__is_checked_out = False
+
+    def return_book(title):
+        for b in Library.__books:
+            b: Book
+            if b.title == title:
+                b.__is_checked_out = True
+
